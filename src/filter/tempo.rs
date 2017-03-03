877202f29c908020f7d1c9963404cd58b5ad8f6e
polyfloyd <floyd@polyfloyd.net>
2017-03-03 12:50:54 +0100

    Slightly improve the phase vocoder
    - Now no longer limited to multiples of .5

--- src/filter/tempo.rs
+++ src/filter/tempo.rs
@@ -5,55 +5,47 @@ pub struct PhaseVocoder<S>
     where S: stft::Stft {
     input: S,
     ratio: f64,
-    counter: f64,
+    // Stores how much of the accum[0] has been consumed in previous iterations.
+    consumption: f64,
     accum: collections::VecDeque<Vec<Vec<f64>>>,
-    output: collections::VecDeque<Vec<Vec<f64>>>,
 }
 
 impl<S> iter::Iterator for PhaseVocoder<S>
     where S: stft::Stft {
     type Item = S::Item;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.output.len() == 0 {
-            while self.counter < 1.0 {
-                match self.input.next() {
-                    Some(block) => {
-                        self.accum.push_back(block);
-                        self.counter += 1.0 / self.ratio;
-                    },
-                    None => return None, // TODO: process leftovers.
-                };
-            }
-            assert!(self.accum.len() > 0);
+        assert!(0.0 <= self.consumption && self.consumption < 1.0);
 
-            if self.ratio < 1.0 {
-                // Slow down the tempo. Input blocks are replicated to create output blocks.
-                let num_blocks = (1.0 / self.ratio).floor() as usize;
-                for block in iter::repeat(self.accum.pop_front().unwrap()).take(num_blocks) {
-                    self.output.push_back(block);
-                }
+        // The ratio is also the number of blocks that we should use.
+        let num_blocks = (self.consumption + self.ratio).ceil() as usize;
+        assert!(num_blocks >= 1);
 
-            } else if self.ratio >= 1.0 {
-                // Accelerate the tempo. Input blocks are merged into less blocks.
-                let num_channels = self.accum[0].len();
-                let window_size = self.input.window_size();
-                let avg = 1.0 / self.accum.len() as f64;
-                let block = self.accum
-                    .drain(..)
-                    .fold(vec![vec![0.0; window_size]; num_channels], |mut sum, block| {
-                        for (ch, channel) in block.into_iter().enumerate() {
-                            for (i, bin_half) in channel.into_iter().enumerate() {
-                                sum[ch][i] += bin_half * avg;
-                            }
-                        }
-                        sum
-                    });
-                self.output.push_back(block);
-            }
-            self.counter %= 1.0;
+        self.accum.extend(self.input.by_ref().take(self.ratio.ceil() as usize));
+        let num_blocks_available = cmp::min(num_blocks, self.accum.len());
+        if num_blocks_available == 0 {
+            return None;
         }
 
-        Some(self.output.pop_front().unwrap())
+        let num_channels = self.accum[0].len();
+        let output_block = self.accum.iter()
+            .take(num_blocks_available)
+            .fold(vec![vec![0.0; self.input.window_size()]; num_channels], |mut sum, block| {
+                let avg = 1.0 / num_blocks_available as f64;
+                for (ch, channel) in block.into_iter().enumerate() {
+                    for (i, bin_half) in channel.into_iter().enumerate() {
+                        // TODO: Better interpolation between blocks.
+                        sum[ch][i] += bin_half * avg;
+                    }
+                }
+                sum
+            });
+
+        // Remove the blocks that have been fully consumed.
+        self.consumption = self.consumption + self.ratio;
+        self.accum.drain(0..self.consumption.floor() as usize);
+        self.consumption %= 1.0;
+
+        Some(output_block)
     }
 }
 
@@ -79,9 +71,8 @@ pub trait AdjustTempo: stft::Stft + Sized {
         PhaseVocoder {
             input: self,
             ratio: ratio,
-            counter: 0.0,
+            consumption: 0.0,
             accum: collections::VecDeque::new(),
-            output: collections::VecDeque::new(),
         }
     }
 }
